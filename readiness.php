e70b55c1220bf75a4716307fd32e64ef091d3bd2
Maya Baya <maya@example.org>
2022-05-20 08:37:20 +0200

    fix: Readiness should not cut off traffic when Wordpress is not installed

--- readiness.php
+++ readiness.php
@@ -1,5 +1,10 @@
 <?php declare(strict_types=1);
 
+if (is_file('wp-admin/install.php')) {
+    echo "OK, but requires installation";
+    exit(0);
+}
+
 require_once __DIR__ . '/wp-load.php';
 wp();
 
